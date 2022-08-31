c862302a7d98038f80700aa0bbc086e9d56e0d41
Nele Liis Sams <nesams@ttu.ee>
2022-08-31 10:12:14 +0300

    fibonacci

---
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -0,0 +1,18 @@
+package ee.ttu.algoritmid.fibonacci;
+
+public class AL01A {
+    /**
+     * Compute the Fibonacci sequence number.
+     * @param n The number of the sequence to compute.
+     * @return The n-th number in Fibonacci series.
+     */
+    public String iterativeF(int n) {
+        int a = 1, b = 1, c;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return Integer.toString(a);
+    }
+}
