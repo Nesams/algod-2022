2706da034d65aa1d3ac9fff9ec5be5d8002a2c5a
Nele Liis Sams <nesams@ttu.ee>
2022-08-31 15:40:22 +0300

    fibonacci2

--- AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
+++ AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
@@ -1,7 +1,8 @@
 package ee.ttu.algoritmid.fibonacci;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.concurrent.TimeUnit;
+import java.math.RoundingMode;
 
 public class AL01B {
 
@@ -11,16 +12,36 @@ public class AL01B {
      * @return The time estimate or exact time in YEARS.
      */
     public static String timeToComputeRecursiveFibonacci(int n) {
-        double start = System.currentTimeMillis();
-        recursiveF(n);
-        double end = System.currentTimeMillis();
+        long start = System.nanoTime();
+        recursiveF(5);
+        long end = System.nanoTime();
 
-        double duration = end - start;
+        BigDecimal duration = BigDecimal.valueOf((end - start));
 
-        double durationInYears = duration / 307584 / 100000;
+        BigDecimal lines = iterativeF(n).multiply(BigDecimal.valueOf(3)).subtract(BigDecimal.valueOf(2));
 
-        return Double.toString(durationInYears);
+        BigDecimal finalNanoSeconds = duration.multiply(lines).divide(iterativeF(5), 10, RoundingMode.HALF_EVEN);
+
+        BigDecimal durationInSeconds = finalNanoSeconds.divide(BigDecimal.valueOf(1000000000),10, RoundingMode.HALF_EVEN);
+        BigDecimal durationInMonths = durationInSeconds.divide(BigDecimal.valueOf(26282888), 10, RoundingMode.HALF_EVEN);
+        BigDecimal durationInYears = durationInMonths.divide(BigDecimal.valueOf(12), 10, RoundingMode.HALF_EVEN);
+
+        return durationInYears.toString();
     }
+
+    public static BigDecimal iterativeF(int n) {
+        if(n <= 1) {
+            return BigDecimal.valueOf(n);
+        }
+        BigDecimal a = BigDecimal.ONE, b = BigDecimal.ONE, c;
+        for (int i = 2; i <= n; i++) {
+            c = a.add(b);
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+
 
     /**
      * Compute the Fibonacci sequence number recursively.
@@ -35,6 +56,6 @@ public class AL01B {
     }
 
     public static void main(String[] args) {
-        System.out.println(timeToComputeRecursiveFibonacci(33));
+        System.out.println(timeToComputeRecursiveFibonacci(12));
     }
 }
